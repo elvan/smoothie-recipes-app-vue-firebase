e3fd8aed02a0f070a6250b70c9be1ef853b7b2d7
Elvan Hidayat <elvanhd@gmail.com>
2023-02-26 12:14:10 +0700

    add-smoothie component

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import AddSmoothie from '@/components/AddSmoothie';
 import Index from '@/components/Index';
 import Vue from 'vue';
 import Router from 'vue-router';
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/add-smoothie',
+      name: 'AddSmoothie',
+      component: AddSmoothie
     }
   ]
 });
